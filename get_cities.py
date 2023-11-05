3815c2614aa0515ffee765d1c3e168d93d17af3d
Oscar <o.zwagers@icloud.com>
2023-11-06 00:03:04 +0100

    query all the properties from the wikidata. the results are in the data directory

--- get_cities.py
+++ get_cities.py
@@ -1,3 +1,5 @@
+import pprint
+
 import requests
 import json
 
@@ -6,34 +8,48 @@ def get_cities(population_size: int = 125000) -> list[dict[str, dict[str, str]]]
     """
     With a SPARQL query, get all cities with a population of over 125000. The city name and
     population size are returned.
+    :return:
     """
     url = "https://query.wikidata.org/sparql"
-    query = (
-        """
-    SELECT ?city ?cityLabel  ?population
-    WHERE
-    {
-      ?city wdt:P31 wd:Q515.
-      ?city wdt:P1082 ?population.
-      FILTER(?population > """
-        + str(population_size)
-        + """)
-      SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-    }
+    query = f"""
+    prefix schema: <http://schema.org/>
+    PREFIX wikibase: <http://wikiba.se/ontology#>
+    PREFIX wd: <http://www.wikidata.org/entity/>
+    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+
+    SELECT DISTINCT ?cid ?city ?article_en ?article_nl
+    WHERE {{
+        ?cid wdt:P31 wd:Q515 .
+        ?cid wdt:P1082 ?population .
+        ?cid rdfs:label ?city filter (lang(?city) = "en") .
+        ?article_en schema:about ?cid .
+        ?article_en schema:inLanguage "en" .
+        ?article_nl schema:about ?cid .
+        ?article_nl schema:inLanguage "nl"
+        FILTER (
+            SUBSTR(str(?article_en), 1, 25) = "https://en.wikipedia.org/" 
+            && SUBSTR(str(?article_nl), 1, 25) = "https://nl.wikipedia.org/"
+            && ?population > {population_size}
+        )
+    }} 
     """
-    )
+
     headers = {
         'Accept': 'application/sparql-results+json,*/*;q=0.9',
         'User-Agent': 'python-requests/2.24.0',
         'Content-Type': 'application/x-www-form-urlencoded',
     }
-    data = {'query': query, 'format': 'json'}
+    data = {
+        'query': query,
+        'format': 'json'
+    }
     response = requests.post(url, headers=headers, data=data)
     result = [
         {
-            "name": item['cityLabel']['value'],
-            "population": item['population']['value'],
-            "uri": item['city']['value'],
+            "uri": item['cid']['value'],
+            "name": item['city']['value'],
+            "url_en": item['article_en']['value'],
+            "url_nl": item['article_nl']['value']
         }
         for item in response.json()['results']['bindings']
     ]
@@ -42,8 +58,7 @@ def get_cities(population_size: int = 125000) -> list[dict[str, dict[str, str]]]
 
 def main():
     cities = get_cities(125000)
-
-    with open("./data/cities.json", "w") as file:
+    with open("data/cities.json", "w") as file:
         json.dump(cities, file, indent=4)
 
 
--- get_properties.py
+++ get_properties.py
@@ -1,6 +1,9 @@
 import sys
 import json
-from typing import Literal
+import time
+from typing import Literal, NamedTuple
+from dataclasses import dataclass, asdict
+from dataclasses_json import dataclass_json
 import requests
 from tqdm import tqdm
 from more_itertools import chunked
@@ -9,121 +12,131 @@ from pprint import pprint
 # use user agent to prevent API from blocking requests
 USER_AGENT = 'infobox_completion_bot/1.0 (b.b.j.overbeek@student.rug.nl)'
 
-# set to True to get properties for each city instead of all properties
-PER_CITY = True
+
+@dataclass_json
+@dataclass
+class CityProperty:
+    """A property with its id, and label in English and Dutch"""
+    id: str
+    label_en: str
+    label_nl: str
 
 
-def get_entity_properties(entity_id: str) -> set[str]:
-    """Get all property ids from an entity (id)"""
+@dataclass_json
+@dataclass
+class Property:
+    """A property with its id, and label in English and Dutch"""
+    id: str
+    label_en: str
+    label_nl: str
 
-    # make request to wikidata API
-    url = f'https://www.wikidata.org/wiki/Special:EntityData/{entity_id}.json'
-    params = {
-        'User-Agent': USER_AGENT,
-        'format': 'json',
-        'max_retries': 10,
-        'dely_between_retries': 2,
+    absolute_frequency: int
+    relative_frequency: float
+
+
+@dataclass
+class City:
+    """A city with its name, population, and wikidata URI"""
+    name: str
+    uri: str
+    url_en: str
+    url_nl: str
+
+    properties: dict[str, CityProperty]
+
+
+def get_entity_properties_and_labels(entity_id: str) -> dict[str, CityProperty]:
+    """Get all property ids from an entity (id) using sparql"""
+
+    url = 'https://query.wikidata.org/sparql'
+    query = f"""
+        PREFIX wd: <http://www.wikidata.org/entity/>
+        PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX wikibase: <http://wikiba.se/ontology#>
+        
+        SELECT DISTINCT ?property ?propertyLabel_en ?propertyLabel_nl WHERE {{
+          wd:{entity_id} ?p ?statement .
+          ?property wikibase:directClaim ?p .
+          
+          OPTIONAL {{
+            ?property rdfs:label ?propertyLabel_en .
+            FILTER (lang(?propertyLabel_en) = "en")
+          }}
+          OPTIONAL {{
+            ?property rdfs:label ?propertyLabel_nl .
+            FILTER (lang(?propertyLabel_nl) = "nl")
+          }}
+        }}
+    """
+
+    headers = {
+        'Accept': 'application/sparql-results+json,*/*;q=0.9',
+        'User-Agent': 'python-requests/2.24.0',
+        'Content-Type': 'application/x-www-form-urlencoded',
     }
-    results_json = requests.get(url, params, timeout=10).json()
+    data = {'query': query, 'format': 'json'}
+    response = requests.post(url, headers=headers, data=data)
 
-    # get all properties from the entity
-    entity_properties = set(results_json['entities'][entity_id]['claims'])
+    if response.status_code != 200:
+        time.sleep(5)
+        return get_entity_properties_and_labels(entity_id)
 
-    return entity_properties
-
-
-def get_property_names(
-    properties: dict[str, dict], lang: Literal['en', 'nl', 'both'] = 'both'
-) -> dict[str, dict[Literal['en', 'nl'], str]]:
-    """Retrieve the name of properties in dict in a given language. Adds the
-    name in the language to the dict and returns the dict."""
-    # convert both to proper format for request
-    if lang == 'both':
-        lang = 'en|nl'
-
-    # make request per 50 properties (this is the limit)
-    for properties_batch in tqdm(
-        chunked(properties, 50),
-        desc='Getting property names',
-        total=len(properties) // 50,
-        leave=False,
-    ):
-        # convert property_ids to string
-        property_ids_str = '|'.join(properties_batch)
-
-        # make request to wikidata API
-        url = f'https://www.wikidata.org/w/api.php?action=wbgetentities&props=labels&ids={property_ids_str}&languages={lang}'
-        params = {
-            'format': 'json',
-            'max_retries': 10,
-            'dely_between_retries': 2,
-        }
-        results_json = requests.get(url, params, timeout=10).json()
-
-        # add result to final dict
-        for property_id, value in results_json['entities'].items():
-            for lang in value['labels']:
-                properties[property_id][lang] = value['labels'][lang]['value']
-
-    return properties
+    return {
+        item['property']['value']: CityProperty(
+            id=item['property']['value'],
+            label_en=item.get('propertyLabel_en', None)["value"] if item.get(
+                'propertyLabel_en', None
+            ) else None,
+            label_nl=item.get('propertyLabel_nl', None)["value"] if item.get(
+                'propertyLabel_nl', None)
+            else None,
+        )
+        for item in response.json()['results']['bindings']
+    }
 
 
-def create_all_properties_dict(cities: list[dict[str, str]]) -> dict[str, dict]:
+def get_properties_per_city(cities: list[City]) -> dict[str, dict[str, CityProperty]]:
+    """Get all properties for each city"""
+
+    properties_per_city = dict()
+    for city in tqdm(cities, desc='Getting properties per city'):
+        entity_id = city.uri.split('/')[-1]
+        properties_per_city[entity_id] = get_entity_properties_and_labels(entity_id)
+
+    return properties_per_city
+
+
+def create_all_properties_dict(
+        properties_per_city: dict[str, dict[str, CityProperty]]
+) -> dict[str, Property]:
     """Create a dictionary with all properties, their translation, and their
     absolute and relative frequency.
     """
 
     # get a set of all occuring properties and add key to dict for each
-    properties = dict()
-    for city in tqdm(cities, desc='Getting properties', leave=False):
-        city_property_ids = get_entity_properties(city['uri'].split('/')[-1])
-        for property_id in city_property_ids:
-            if properties.get(property_id, None) is None:
-                properties[property_id] = {
-                    'en': '',
-                    'nl': '',
-                    'absolute_frequency': 1,
-                    'relative_frequency': 0,
-                }
+    all_properties = dict()
+    for city, properties in tqdm(
+            properties_per_city.items(), desc='Getting properties', leave=False
+    ):
+        for property_id, value in properties.items():
+            if all_properties.get(property_id, None) is None:
+                all_properties[property_id] = Property(
+                    id=property_id,
+                    label_en=value.label_en,
+                    label_nl=value.label_nl,
+                    absolute_frequency=1,
+                    relative_frequency=0
+                )
             else:
-                properties[property_id]['absolute_frequency'] += 1
+                all_properties[property_id].absolute_frequency += 1
 
     # calculate relative frequency
-    for property_id, value in properties.items():
-        value['relative_frequency'] = value['absolute_frequency'] / len(cities)
+    for property_id, value in all_properties.items():
+        all_properties[property_id].relative_frequency = \
+            value.absolute_frequency / len(properties_per_city)
 
-    # get the Dutch and English name for each property
-    properties = get_property_names(properties)
-
-    return properties
-
-
-def create_cities_properties_dict(
-    cities: list[dict[str, str]], all_properties_path: str
-) -> dict[str, dict]:
-    """Create a dictionary with all cities, their properties, and the
-    translation of these properties. Uses the all_properties json to limit
-    requests to the server.
-    """
-
-    city_properties = dict()
-
-    with open(all_properties_path, 'r', encoding='utf-8') as inp:
-        all_properties = json.load(inp)
-
-    for city in tqdm(cities, desc='Getting properties per city', leave=False):
-        city_properties[city['name']] = dict()
-        city_property_ids = get_entity_properties(city['uri'].split('/')[-1])
-        for property_id in city_property_ids:
-            city_properties[city['name']][property_id] = dict()
-            city_properties[city['name']][property_id]['nl'] = all_properties[
-                property_id
-            ].get('nl', '')
-            city_properties[city['name']][property_id]['en'] = all_properties[
-                property_id
-            ].get('en', '')
-
-    return city_properties
+    return all_properties
 
 
 def main(argv: list[str]):
@@ -131,34 +144,36 @@ def main(argv: list[str]):
     Run with: python get_properties.py ./data/cities.json ./data/all_properties.json
     To get properties per city, set PER_CITY to True and run again.
     """
-    # open file with cities and their wikidata URI's
     with open(argv[1], 'r', encoding='utf-8') as inp:
-        cities = json.load(inp)
+        cities = [
+            City(
+                name=city['name'],
+                uri=city['uri'],
+                url_en=city['url_en'],
+                url_nl=city['url_nl'],
+                properties=dict()
+            ) for city in json.load(inp)
+        ]
 
-    if PER_CITY:
-        # create properties dict with properties per city
-        properties = create_cities_properties_dict(cities, './data/all_properties.json')
-
-    else:
-        # create properties dictionary with all properties and their frequency
-        properties = create_all_properties_dict(cities)
-
-    # pprint(properties)
+    properties_per_city = get_properties_per_city(cities)
+    all_properties = create_all_properties_dict(properties_per_city)
 
     # write properties to file
     with open(argv[2], 'w', encoding='utf-8') as out:
-        json.dump(properties, out, indent=4)
+        json.dump(properties_per_city, out, indent=4, default=lambda x: x.to_dict())
 
-    if PER_CITY:
-        print(
-            f'Done, properties for {len(cities)} cities written to file: {argv[2]}',
-            file=sys.stderr,
-        )
-    else:
-        print(
-            f'Done, {len(properties)} properties written to file: {argv[2]}',
-            file=sys.stderr,
-        )
+    with open(argv[3], 'w', encoding='utf-8') as out:
+        json.dump(all_properties, out, indent=4, default=lambda x: x.to_dict())
+
+    print(
+        f'Done, properties for {len(properties_per_city)} cities written to file: {argv[2]}',
+        file=sys.stderr,
+    )
+
+    print(
+        f'Done, {len(all_properties)} properties written to file: {argv[3]}',
+        file=sys.stderr,
+    )
 
 
 if __name__ == '__main__':
