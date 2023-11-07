c9e685b62be8227ed13de1a709aa5f0026c036cf
Björn Overbeek <bbjoverbeek@gmail.com>
2023-11-07 03:03:16 +0100

    Add proper saving

--- compute_euclidean_distance.py
+++ compute_euclidean_distance.py
@@ -3,6 +3,9 @@ from tqdm import tqdm
 import json
 import numpy as np
 from transformers import pipeline
+import warnings
+
+warnings.filterwarnings('ignore')
 
 # Load the model and tokenizer
 pipe = pipeline('feature-extraction', model='xlm-roberta-base', device=0)
@@ -12,10 +15,7 @@ def compute_similarity(
     token1: str,
     token2: str,
 ) -> float:
-    """Extracts word embeddings from an LLM (default xlm-roberta-base)
-    at a given layer and returns their euclidean distance. If cosine_similarity
-    is True, returns their cosine similarity.
-    """
+    """Extracts word embeddings from an LLM and returns their euclidean distance."""
 
     # Get embeddings for the input strings
     emb1 = np.array(pipe(token1)).mean(axis=1)
@@ -32,15 +32,17 @@ def main():
 
     euclidean_dist_mapping = dict()
 
-    for property_id1, value1 in tqdm(all_properties.items()):
-        euclidean_dist_mapping[(property_id1, value1['label_en'])] = dict()
-        for property_id2, value2 in tqdm(all_properties.items()):
-            euclidean_dist_mapping[(property_id1, value1['label_en'])][
-                (property_id2, value2['label_nl'])
-            ] = compute_similarity(value1['label_en'], value2['label_nl'])
+    for property_id1, value1 in tqdm(all_properties.items(), leave=False):
+        euclidean_dist_mapping[property_id1] = dict()
+        for property_id2, value2 in tqdm(all_properties.items(), leave=False):
+            euclidean_dist_mapping[property_id1][property_id2] = compute_similarity(
+                value1['label_en'], value2['label_nl']
+            )
 
-    with open('data/all-properties-dist.json', 'r', encoding='utf-8') as outp:
+    with open('data/all-properties-dist.json', 'w', encoding='utf-8') as outp:
         json.dump(euclidean_dist_mapping, outp, indent=4)
+
+    print('Done!')
 
 
 if __name__ == '__main__':
