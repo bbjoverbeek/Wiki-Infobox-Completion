c0dc2f78ac2b28c2a46f1028d7f238c28807c5f0
Björn Overbeek <bbjoverbeek@gmail.com>
2023-11-12 23:27:29 +0100

    Complete code

--- test_system.py
+++ test_system.py
@@ -1,189 +1,93 @@
 import json
-import argparse
-import numpy as np
 from tqdm import tqdm
-import sys
-from util import CityProperty, Property, City
-from sklearn.metrics import classification_report, precision_score
+from util import CityProperty
+from compute_threshold import create_similarity_mapping
+
+SimilarityMapping = dict[str, dict[str, float]]
 
 
-def parse_args():
-    """Parse the args to determine whether to test the threshold method, or
-    robust method, and to test on all properties or properties per city.
-    """
-    parser = argparse.ArgumentParser()
+def compute_similarities_per_city(
+    prop_per_city: dict[str, dict[str, CityProperty]],
+    similarity_mapping: SimilarityMapping,
+) -> dict[str, SimilarityMapping]:
+    """Creates a similarity mapping per city"""
+
+    similarity_mapping_per_city = dict()
+
+    for city_id, city in prop_per_city.items():
+        similarity_mapping_per_city[city_id] = dict()
+        for property_id1, value1 in city.items():
+            similarity_mapping_per_city[city_id][property_id1] = dict()
+            for property_id2, value2 in city.items():
+                similarity_mapping_per_city[city_id][property_id1][
+                    property_id2
+                ] = similarity_mapping[property_id1][property_id2]
+
+    return similarity_mapping_per_city
 
 
-def test_threshold_per_city():
-    """"""
-    pass
-
-
-def test_threshold_all_properties():
-    """"""
-    pass
-
-
-def test_robust_per_city(
-    prop_per_city: dict[str, dict[str, CityProperty]], from_lang: str, to_lang: str
+def test_per_city(
+    prop_per_city: dict[str, dict[str, CityProperty]],
+    similarity_mapping: SimilarityMapping,
 ):
-    """Computes the property similarity between all properties of each city,
-    and determines which property aligns best witht the other translation"""
+    """Tests the word embeddings per city, and returns the sum of all cities"""
+    similarities_per_city = compute_similarities_per_city(
+        prop_per_city, similarity_mapping
+    )
 
-    for city_id, city_properties in prop_per_city.items():
-        property_map = dict()
-        for property_id, property_langs in city_properties.items():
-            if property_langs[from_lang] == "":
-                continue
-            property_map[(property_id, property_langs[from_lang])] = ""
+    sum_positions = {idx: 0 for idx in range(1, len(similarity_mapping) + 1)}
 
-            # compute_similarity(
-            #     property_, city_properties[property_], prop_per_city, from_lang, to_lang
-            # )
+    for city, similarities in tqdm(similarities_per_city.items(), leave=False):
+        city_positions = test_all_properties(similarities)
+        for key in city_positions:
+            sum_positions[key] += city_positions[key]
 
-    print(property_map)
+    # print(sum(sum_positions.values()))
 
-
-def test_robust_properties(properties) -> list[int]:
-    """Uses the property embeddings to test the compute_metric function"""
-    predictions: list[int] = []
-
-    correct_relative: float = 0.0
-    incorrect_relative: float = 0.0
-
-    correct_similarities: list[float] = []
-    incorrect_similarities: list[float] = []
-
-    for property_id, value in tqdm(properties.items()):
-        similarities = {
-            pid: compute_similarity(value["emb_nl"], val["emb_en"])
-            for pid, val in properties.items()
-        }
-        if min(similarities, key=similarities.get) == property_id:
-            predictions.append(1)
-            correct_relative += properties[property_id]["relative_frequency"]
-            correct_similarities.append(min(similarities.values()))
-        else:
-            predictions.append(0)
-            incorrect_relative += properties[property_id]["relative_frequency"]
-            incorrect_similarities.append(min(similarities.values()))
-
-    print(f"Correct relative: {correct_relative}")
-    print(f"Incorrect relative: {incorrect_relative}")
-
-    print(f"highest correct similarity: {max(correct_similarities)}")
-    print(f"average correct similarity: {np.mean(correct_similarities)}")
-
-    print(f"lowest incorrect similarity: {min(incorrect_similarities)}")
-    print(f"average incorrect similarity: {np.mean(incorrect_similarities)}")
-
-    return predictions
+    return sum_positions
 
 
-def compute_similarity(emb1: list[float], emb2: list[float]) -> float:
-    """Computes the similarity between two embeddings"""
-    # convert lists to np arrays
-    emb1 = np.array(emb1)
-    emb2 = np.array(emb2)
+def test_all_properties(similarity_mapping: SimilarityMapping) -> dict[int, int]:
+    """Uses the property embeddings to rank all the similarities"""
 
-    # cosine similarity
-    # similarity = np.dot(emb1, emb2) / (np.linalg.norm(emb1) * np.linalg.norm(emb2))
+    positions = {idx: 0 for idx in range(1, len(similarity_mapping) + 1)}
 
-    # euclidean distance
-    similarity = np.linalg.norm(emb1 - emb2)
+    for property_id, value in similarity_mapping.items():
+        # sort by embedding similarity
+        ordered = sorted(value, key=value.get)
+        position = ordered.index(property_id) + 1
+        positions[position] = positions.get(position, 0) + 1
 
-    return similarity
-
-
-def create_similarity_mapping(
-    properties: dict[str, dict]
-) -> dict[str, dict[str, float]]:
-    """Computes the similarity between all property embeddings"""
-
-    similarity_mapping = dict()
-    for property_id1, value1 in tqdm(
-        properties.items(), leave=False, desc="creating similarity mapping"
-    ):
-        similarity_mapping[property_id1] = dict()
-        for property_id2, value2 in properties.items():
-            similarity_mapping[property_id1][property_id2] = compute_similarity(
-                value1["emb_nl"], value2["emb_en"]
-            )
-
-    # {
-    #     "P31": {
-    #         "P23": 0.012314,
-    #         "P31": 0.0013
-    #      }
-    # }
-
-    return similarity_mapping
+    return positions
 
 
-def find_threshold(
-    similarity_mapping: dict[str, dict[str, float]],
-    min_precision: float,
-    increment: float = 0.0005,
-) -> float:
-    """Finds the highest similarity threshold that has a minimum precision of
-    min_precision
-    """
-    # create similarity mapping:
+def main():
+    """Tests the extracted word embeddings extracted from all-properties.json"""
 
-    threshold = 0.5
-    precision = 1.0
-
-    while precision > min_precision:
-        prev_threshold = threshold
-        threshold += increment
-        labels = []
-        predictions = []
-        for property_id1, similarities in similarity_mapping.items():
-            for property_id2, similarity in similarities.items():
-                labels.append(property_id1 == property_id2)
-                predictions.append(similarity < threshold)
-
-        precision = precision_score(labels, predictions, zero_division=0)
-        print(f"precision: {precision:.3f} with threshold: {threshold:.3f}")
-
-    return prev_threshold
-
-
-def main(argv: list[str]):
-    """Tests the property similarity method in two different ways: robust and
-    threshold.
-    """
-    # with open('data/properties-per-city.json', 'r', encoding='utf-8') as inp:
-    #     prop_per_city = json.load(inp)
-
-    model = "xlm-roberta-base"
-
-    with open(
-        f"./data/all-properties-with-emb/{model}.json", "r", encoding="utf-8"
-    ) as inp:
+    with open("./data/all-properties-with-emb.json", "r", encoding="utf-8") as inp:
         all_properties = json.load(inp)
+    with open("./data/properties-per-city.json", "r", encoding="utf-8") as inp:
+        properties_per_city = json.load(inp)
 
-    # similarity_mapping = create_similarity_mapping(all_properties)
-    # with open(f"./data/similarity-mappings/{model}.json", "w", encoding="utf-8") as out:
-    #     json.dump(similarity_mapping, out)
+    try:
+        with open("./data/similarity-mappings.json", "r", encoding="utf-8") as inp:
+            similarity_mapping = json.load(inp)
+    except FileNotFoundError:
+        similarity_mapping = create_similarity_mapping(all_properties)
+        with open("./data/similarity-mappings.json", "w", encoding="utf-8") as out:
+            json.dump(similarity_mapping, out)
 
-    with open(f"./data/similarity-mappings/{model}.json", "r", encoding="utf-8") as inp:
-        similarity_mapping = json.load(inp)
+    positions = test_all_properties(similarity_mapping)
+    positions_city_sum = test_per_city(properties_per_city, similarity_mapping)
 
-    # test_robust_per_city(prop_per_city, from_lang='en', to_lang='nl')
+    print("These are the positions of all the similarities:")
+    print(positions)
 
-    # predictions = test_robust_properties(all_properties)
+    print("---" * 20)
 
-    # print()
-    # print(
-    #     classification_report(
-    #         [1 for x in range(len(predictions))], predictions, zero_division=0
-    #     ),
-    # )
-
-    threshold = find_threshold(similarity_mapping, 0.8)
-    print(f"Found threshold: {threshold:.3f}")
+    print("These are the positions of all the similarities:")
+    print(positions_city_sum)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
